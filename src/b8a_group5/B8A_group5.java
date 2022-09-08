b59b90bad4e2a97edb25c7b84b93a4756b6d3d52
razan <razan@10.24.223.72>
2022-09-08 12:11:33 +0300

    first commit

---
+++ src/b8a_group5/B8A_group5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b8a_group5;
+
+/**
+ *
+ * @author razan
+ */
+public class B8A_group5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
