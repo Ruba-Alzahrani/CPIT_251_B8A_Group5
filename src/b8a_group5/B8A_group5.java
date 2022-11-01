852747e24fc19f1d3d76540c569286c496fc0a82
RazanAzzhrani <razansaeed122@gmail.com>
2022-11-01 09:09:46 +0300

    Razan Azzhrani name was added..

--- src/b8a_group5/B8A_group5.java
+++ src/b8a_group5/B8A_group5.java
@@ -11,16 +11,54 @@ public class B8A_group5 {
         System.out.println("1. Appintments Booking ");
         System.out.println("2. Online consultation  ");
         System.out.println("3. Accessc Patient Profile  ");
-        System.out.println("4. Payemnt"); 
+        System.out.println("4. Blood Test Result"); 
         System.out.println("5. exit");  
         System.out.println("------------------------------------------------------------------");
         System.out.print("Please choose the service ");
         
         int choose = s.nextInt();
         do {
+            if (choose==1){
+                System.out.println("Service: Appintments Booking");
+                
+            }
+            
+            if (choose==2){
+                System.out.println("Service: Online consultation");
+                
+            }
+            
+            if (choose==3){
+                System.out.println("Service: Accessc Patient Profile");
+                
+            }
+            
+            if (choose==4){
+                System.out.println("Service: Blood Test Result");
+                
+            }
+            
+            
         } while (!(choose == 5));
-
 
     }
 
+    public static void Appintments(String A){
+        
+    }
+    
+    public static void consultation(String A){
+        
+    }
+    
+    public static void PatientProfile(String A){
+        
+    }
+    
+    public static void BloodTest(String A){
+        
+    }
+    
+    
+    
 }
