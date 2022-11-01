5b86c6a040fad0e3559a02e53e29262d79ca0cfa
razan <razan@10.24.223.72>
2022-11-01 10:58:34 +0300

    Razan almalki

--- src/b8a_group5/B8A_group5.java
+++ src/b8a_group5/B8A_group5.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class B8A_group5 {
 
     public static void main(String[] args) {
+        String Appintment_information[] = new String[3];
+       
+        Appintment_information[0] = "0- Dr.Razan, 10.11.2022 ,8 PM";
+        Appintment_information[1] = "1- Dr.Ruba, 15.11.2022 ,9 PM";
+        Appintment_information[2] = "2- Dr.Raghad, 29.11.2022 ,7 PM";
         Scanner s = new Scanner(System.in);
 
         System.out.println("---------------- Welcome to Hospital Application ----------------");
@@ -14,13 +19,15 @@ public class B8A_group5 {
         System.out.println("4. Blood Test Result"); 
         System.out.println("5. exit");  
         System.out.println("------------------------------------------------------------------");
-        System.out.print("Please choose the service ");
+        System.out.print("Please choose the service: ");
         
         int choose = s.nextInt();
         do {
             if (choose==1){
                 System.out.println("Service: Appintments Booking");
-                
+                String h=Appintments(s,Appintment_information);
+               
+ 
             }
             
             if (choose==2){
@@ -38,17 +45,29 @@ public class B8A_group5 {
                 
             }
             
+     System.out.println("Please choose other service or 5 to exit: ");
+              choose = s.nextInt();
             
-        } while (!(choose == 5));
+                } while (!(choose == 5));
 
     }
 
-    public static void Appintments(String A){
-        
+    public static String Appintments(Scanner s, String Appintment_information[]) {
+        for (int i = 0; i < Appintment_information.length; i++) {
+            System.out.println("The Appintments are:  ");
+            System.out.println(Appintment_information[i]);
+            System.out.println("-------------------------------------------");
+        }
+        System.out.print("Enter your chosen Appintment (from 0 to 2): ");
+        int choice = s.nextInt();
+        String store = Appintment_information[choice];
+        System.out.print("The selected Appintment has been added to your Profile\n");
+        Appintment_information[choice] = "This Appintment has been reserved! ";  
+        return store;
     }
-    
-    public static void consultation(String A){
-        
+
+    public static void consultation(String A) {
+
     }
     
     public static void PatientProfile(String A){
