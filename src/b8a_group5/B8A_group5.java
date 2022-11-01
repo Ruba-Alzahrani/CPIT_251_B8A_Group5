609d104a04ec37494f5200583b44315253f2fe97
Ruba_Alzahrani <iivxwwi@gmail.com>
2022-11-01 08:30:00 +0300

    Ruba Alzahrani

--- src/b8a_group5/B8A_group5.java
+++ src/b8a_group5/B8A_group5.java
@@ -1,28 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package b8a_group5;
 
-/**
- *
- * @author razan
- */
+package b8a_group5;
+import java.util.Scanner;
+
 public class B8A_group5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-    System.out.println("---------------- Welcome to Hospital Application ----------------");
-    System.out.println("------------------ Please choose the service ---------------------");
-    System.out.println("1. Appintments Booking ");
-    System.out.println("2. Online consultation  ");
-    System.out.println("3. Accessc Patient Profile  ");
-    System.out.println("4. Payemnt");
-    System.out.println("-----------------------------------------------------------------");
-     
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("---------------- Welcome to Hospital Application ----------------");
+        System.out.println("1. Appintments Booking ");
+        System.out.println("2. Online consultation  ");
+        System.out.println("3. Accessc Patient Profile  ");
+        System.out.println("4. Payemnt");        
+        System.out.println("------------------------------------------------------------------");
+        System.out.print("Please choose the service ");
+        
+        int choose = s.nextInt();
+
+
     }
-    
+
 }
